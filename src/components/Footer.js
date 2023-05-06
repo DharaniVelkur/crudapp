81a319e8d094142ec699dea4f184d9bbe6761e8a
Velkur Dharani <dharanivelkur@gmail.com>
2023-05-06 17:29:10 +0530

    ..

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-dark text-light' >
+        <p className='text-center'> Copyright &copy; MyTodosList.com</p>
+      
+    </div>
+  )
+}
+
+export default Footer
